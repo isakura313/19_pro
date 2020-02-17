7b507d0d4f1c599242497faf7a891b697d7c0a6f
Paul <isakura313@gmail.com>
2020-02-17 19:41:24 +0300

    asd

--- classes/User.class.php
+++ classes/User.class.php
@@ -3,5 +3,7 @@
 
 class User
 {
-
+ public $name;
+ protected $email;
+ private $password;
 }
---
+++ classes/Util.php
@@ -0,0 +1,19 @@
+<?php
+
+
+class Util
+{
+    public $folder_name = '19_pro';
+    public static function get_full_css($file){
+
+        $path = "<link rel='stylesheet' href=
+        http://{$_SERVER['HTTP_HOST']}/this->folder_name/{$file}";
+        return $path;
+
+}
+    public static  function get_full_js($file){
+        $path = "http://{$_SERVER['HTTP_HOST']}/19_pro/{$file}";
+        return $path;
+    }
+
+}
