33e33a409f450bf1988355395123b1fd9c5056b7
Andrew Gulin <andrewgulin@MacBook-Air-Andrew.local>
2020-02-19 01:11:22 +0300

    hw

---
+++ classes/Info.php
@@ -0,0 +1,11 @@
+<?php
+
+class Info
+{
+    public function __construct($id, $content, $ordera)
+    {
+        $this->id = $id;
+        $this->content = $content;
+        $this->ordera = $ordera;
+    }
+}
